f27630075ea53113dbe3268f0d9f2e914c641358
imkyuuuuu <imkyuuuuu@gmail.com>
2026-02-01 10:24:51 -0500

    Update service worker cache version to v3

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // PWA cache minimal : offline after first load.
-const CACHE_NAME = "casino-crush-pwa-v2";
+const CACHE_NAME = "casino-crush-pwa-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -36,6 +36,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
