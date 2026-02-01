72d6321450971a8dc0148953476a3c3c5022854c
imkyuuuuu <imkyuuuuu@gmail.com>
2026-02-01 10:42:34 -0500

    Upgrade PWA cache to version 6 and add sprites
    Updated cache version and added new asset sprites.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 // PWA cache minimal : offline after first load.
-const CACHE_NAME = "casino-crush-pwa-v4";
+const CACHE_NAME = "casino-crush-pwa-v6";
+
 const ASSETS = [
   "./",
   "./index.html",
@@ -11,6 +12,14 @@ const ASSETS = [
   "./service-worker.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
+
+  // Slots sprites (si présents)
+  "./assets/sprites/cherry.png",
+  "./assets/sprites/lemon.png",
+  "./assets/sprites/dice.png",
+  "./assets/sprites/chip.png",
+  "./assets/sprites/card.png",
+  "./assets/sprites/diamond.png"
 ];
 
 self.addEventListener("install", (event) => {
@@ -22,8 +31,8 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
     )
   );
   self.clients.claim();
@@ -31,13 +40,6 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
-
-
-
-
-
